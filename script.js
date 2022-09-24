30317c446d4249ee2dbd3f13aa7d5fb215e0a5eb
skwyarspro15 <potatobg648@gmail.com>
2022-09-24 14:51:37 +0800

    Added account authentication and get text function

--- script.js
+++ script.js
@@ -1,7 +1,70 @@
-function connectDocument(documentName) {}
+function enterDocument(documentName) {}
 
 function createDocument(documentName) {}
 
 function getDocumentOwner(documentName) {}
 
-function authenticateUser(userName, password) {}
+async function authenticateUser(username, password) {
+  var result = await makeRequest(
+    "GET",
+    "https://CloudWebV2.skywarspro15.repl.co/login.php?username=" +
+      username +
+      "&password=" +
+      password
+  );
+
+  return result;
+}
+
+function getInputText() {
+  return document.getElementsByClassName("ql-editor")[0].innerHTML;
+}
+
+function makeRequest(method, url) {
+  return new Promise(function (resolve, reject) {
+    let xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.onload = function () {
+      if (this.status >= 200 && this.status < 300) {
+        resolve(xhr.response);
+      } else {
+        reject({
+          status: this.status,
+          statusText: xhr.statusText,
+        });
+      }
+    };
+    xhr.onerror = function () {
+      reject({
+        status: this.status,
+        statusText: xhr.statusText,
+      });
+    };
+    xhr.send();
+  });
+}
+
+function setCookie(cname, cvalue) {
+  const today = new Date();
+  const d = new Date();
+  d.setTime(today.getTime() + 3600000 * 24 * 15);
+  let expires = "expires=" + d.toUTCString();
+  document.cookie =
+    cname + "=" + cvalue + ";" + expires + ";path=/; SameSite=Lax";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
